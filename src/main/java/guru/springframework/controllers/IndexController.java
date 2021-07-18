45ea5d8cae31bfbed4e4cefadef05fd67e046ce8
Mikalai Barysau <stoptherock1@gmail.com>
2021-07-19 01:44:55 +1000

    Add Spring Data JPA Query Methods + change to Java 11

--- src/main/java/guru/springframework/controllers/IndexController.java
+++ src/main/java/guru/springframework/controllers/IndexController.java
@@ -1,13 +1,29 @@
 package guru.springframework.controllers;
 
+import guru.springframework.repositories.CategoryRepository;
+import guru.springframework.repositories.UnitOfMeasureRepository;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class IndexController {
 
+    private CategoryRepository categoryRepository;
+    private UnitOfMeasureRepository unitOfMeasureRepository;
+
+    public IndexController(CategoryRepository categoryRepository, UnitOfMeasureRepository unitOfMeasureRepository) {
+        this.categoryRepository = categoryRepository;
+        this.unitOfMeasureRepository = unitOfMeasureRepository;
+    }
+
     @RequestMapping({"", "/", "/index"})
     public String getIndexPage() {
+        var categoryOptional  = categoryRepository.findByDescription("American");
+        var unitOfMeasureOptional  = unitOfMeasureRepository.findByDescription("Teaspoon");
+
+        System.out.println("Cat id is: " + categoryOptional.get().getId());
+        System.out.println("UOM id is: " + unitOfMeasureOptional.get().getId());
+
         return "index";
     }
 
